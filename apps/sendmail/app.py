e6ac15ca6fb372434bd88a45c43be10d6c89916d
SOLBI <thfqlsla35@gmail.com>
2024-11-15 11:11:22 +0900

    ✨ Route contact, contact_complete 구조 구현
    contact는 메일 폼, contact_complete 메일 보내기와 리다이렉트 기능

--- apps/sendmail/app.py
+++ apps/sendmail/app.py
@@ -1,9 +1,20 @@
-from flask import Flask, request , url_for,make_response, render_template
+from flask import Flask, request , url_for,make_response, render_template, redirect
 import urllib.parse
 
 app = Flask(__name__)
 
+@app.route('/contact') #이메일보내는 폼이 보이는 기능 구현
+def contact():
+    return render_template('contact.html')
 
+@app.route('/contact/complete')
+def contact_complete():
+    if request.method == 'POST': #(방금 들어온 요청에) 너 POST야?
+        #이메일 보내기 코드 작성
+
+        return redirect('contact_complete') #강제로 특정 위치로 보내버리기 , 엔드포인트
+    return render_template('contact_complete.html') #메일 접수 완료한 화면 구성 html 파일 보여줄게 
+ 
 
 
 
